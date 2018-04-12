75bbf1a4e170dbc81c8544356bd908bba3b97887
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-04-12 21:52:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ function theBeatlesPlay(musicians,instruments){
   for(var i = 0; i < musicians.length; i++){
     temp.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  return temp;
 }
 
 function johnLennonFacts(facts){
   var count = 0;
   while(count < facts.length){
-    facts[count].con
+    facts[count] += '!!!';
+    count++;
   }
+  return facts;
 }
